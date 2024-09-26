66487bd690d937670cae92c73066a271b0e130b2
astrobemo <astrobemo@gmail.com>
2024-09-26 15:40:47 +0700

    add cors

--- src/main.js
+++ src/main.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import { prismaClientFavour, prismaClientBlessing, prismaClientGrace } from "./prisma-client.js";
+import cors from "cors";
 
 
 const app = express();
 
+const allowedOrigins = ['http://202.138.247.174'];
 
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
