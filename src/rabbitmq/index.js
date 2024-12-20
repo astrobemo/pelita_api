0146441d939c9bc0cf21d08ee3550c827cc5a2ce
astrobemo <astrobemo@gmail.com>
2024-12-20 11:49:22 +0700

    add log to check rabbtiMq connection

--- src/rabbitmq/index.js
+++ src/rabbitmq/index.js
@@ -1,7 +1,7 @@
 import { consumeMessages as consumeCustomer, rabbitMqParam } from "./customer_consumer.js";
 
-console.log(rabbitMqParam);
 const consumersApp = async () => {
+    console.log('params',rabbitMqParam);
     await consumeCustomer();
     console.log('Consumer is running');
 }
