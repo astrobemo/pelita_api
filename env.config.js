6136e8badbbca198eece1fb83531f3244c8e5efc
astrobemo <astrobemo@gmail.com>
2025-01-17 16:44:32 +0700

    recode env config

--- env.config.js
+++ env.config.js
@@ -1,23 +1,25 @@
 // Export the configuration object
-export default {
+const configuration = {
   apps: [
     {
       name: "pelita-api-app",
       script: "./index.js",
       env: {
-        NODE_ENV: "development", 
+        NODE_ENV: "development",
       },
       env_development: {
-        ENV_FILE: `.env.development`, 
+        ENV_FILE: `.env.development`,
       },
       env_testing: {
-        NODE_ENV: "testing", 
-        ENV_FILE: `.env.testing`, 
+        NODE_ENV: "testing",
+        ENV_FILE: `.env.testing`,
       },
       env_production: {
-        NODE_ENV: "production", 
-        ENV_FILE: `.env.production`, 
+        NODE_ENV: "production",
+        ENV_FILE: `.env.production`,
       },
     },
   ],
 };
+
+export default configuration;
