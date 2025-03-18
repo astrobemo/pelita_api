b1ce211e36c0936281edb5468e37cfa4637bce29
astrobemo <astrobemo@gmail.com>
2025-03-18 10:35:56 +0700

    add barangSKUconsumer

--- src/rabbitmq/consumer.js
+++ src/rabbitmq/consumer.js
@@ -10,8 +10,8 @@ const rabbitMqPassword = process.env.RABBITMQ_PASSWORD;
 
 console.log(rabbitMqUrl, rabbitMqUser, rabbitMqPassword);
 
-const connection =  await connect(`amqp://${rabbitMqUser}:${rabbitMqPassword}@${rabbitMqUrl}:5672/`);
-const channel = await connection.createChannel();
+export const connection =  await connect(`amqp://${rabbitMqUser}:${rabbitMqPassword}@${rabbitMqUrl}:5672/`);
+export const channel = await connection.createChannel();
 
 await channel.consume("webapp_notif", (message) => {
     console.info(message.fields.routingKey);
@@ -21,38 +21,3 @@ await channel.consume("webapp_notif", (message) => {
 
 // await channel.close();
 // await connection.close();
-
-channel.consume("add_barang_master_toko", async (msg) => {
-    if (msg !== null) {
-        const messageContent = msg.content.toString();
-        console.log("Received message:", messageContent);
-        // validate if barang_id and warna_id is known
-
-        try {
-            const company = barangSKU.company;
-            const barangId = barangSKU.barang_id;
-            
-            const existingBarang = await prismaClient[COMPANY[company]].findMany({
-                where: {
-                    barang_id_master: barangId
-                }
-            });
-
-            if(existingBarang.length === 0){
-                
-                await prismaClient[COMPANY[company]].create({
-                    data: {
-                        barang_id_master: barangId
-                    }
-                });
-                return;
-            }
-
-            channel.ack(msg);
-
-        } catch (error) {
-            console.error(error);
-            channel.nack(msg);
-        }
-    }
-});
