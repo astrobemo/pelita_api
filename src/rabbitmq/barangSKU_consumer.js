0ade1658156ba706a3dd41484e9e67d36c460c3d
astrobemo <astrobemo@gmail.com>
2025-03-18 10:49:59 +0700

    integrate connection js to other

--- src/rabbitmq/barangSKU_consumer.js
+++ src/rabbitmq/barangSKU_consumer.js
@@ -1,41 +1,44 @@
 import {connection, channel} from "./connection";
 
-channel.consume("add_barang_master_toko", async (msg) => {
-
+export const barangSKUassigned = async () =>{
     if(!connection){
         throw new Error("RabbitMQ connection is not established");
     }
 
-    if (msg !== null) {
-        const messageContent = msg.content.toString();
-        console.log("Received message:", messageContent);
-        // validate if barang_id and warna_id is known
-
-        try {
-            const company = barangSKU.company;
-            const barangId = barangSKU.barang_id;
-            
-            const existingBarang = await prismaClient[COMPANY[company]].findMany({
-                where: {
-                    barang_id_master: barangId
-                }
-            });
-
-            if(existingBarang.length === 0){
+    channel.consume("add_barang_master_toko", async (msg) => {
+    
+    
+        if (msg !== null) {
+            const messageContent = msg.content.toString();
+            console.log("Received message:", messageContent);
+            // validate if barang_id and warna_id is known
+    
+            try {
+                const company = barangSKU.company;
+                const barangId = barangSKU.barang_id;
                 
-                await prismaClient[COMPANY[company]].create({
-                    data: {
+                const existingBarang = await prismaClient[COMPANY[company]].findMany({
+                    where: {
                         barang_id_master: barangId
                     }
                 });
-                return;
+    
+                if(existingBarang.length === 0){
+                    
+                    await prismaClient[COMPANY[company]].create({
+                        data: {
+                            barang_id_master: barangId
+                        }
+                    });
+                    return;
+                }
+    
+                channel.ack(msg);
+    
+            } catch (error) {
+                console.error(error);
+                channel.nack(msg);
             }
-
-            channel.ack(msg);
-
-        } catch (error) {
-            console.error(error);
-            channel.nack(msg);
         }
-    }
-});
+    });
+}
--- src/rabbitmq/index.js
+++ src/rabbitmq/index.js
@@ -1,7 +1,6 @@
-import { consumeMessages as consumeCustomer, rabbitMqParam } from "./customer_consumer.js";
+import { consumeMessages as consumeCustomer } from "./customer_consumer.js";
 
 const consumersApp = async () => {
-    console.log('params',rabbitMqParam);
     await consumeCustomer();
     console.log('Consumer is running');
 }
