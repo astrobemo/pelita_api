e36339793b2a7993b7b4030808f003f0e1dfc19f
astrobemo <astrobemo@gmail.com>
2025-05-08 10:38:46 +0700

    update loadENv.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import app from "./src/main.js";
 import { ENVIRONMENT, PORT } from "./config/loadEnv.js";
-import { consumersApp } from "./src/rabbitmq/index.js";
+// import { consumersApp } from "./src/rabbitmq/index.js";
 
 console.log(`Environment: ${ENVIRONMENT}`);
 const port = PORT || 3000;
@@ -9,4 +9,4 @@ app.listen(port, () => {
     console.log(`Server is running ${ENVIRONMENT} on http://localhost:${port}`);
 });
 
-consumersApp();
+// consumersApp();
